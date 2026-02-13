6c77252fe6b9162e4a6d7c9f52c9b5e72718816b
JhonnyRivers <68939436+JhonnyRivers@users.noreply.github.com>
2026-02-12 21:42:11 -0300

    coisas

---
+++ Scripts/ASS_ObjectSpawner.lua
@@ -0,0 +1,36 @@
+ASS_ObjectSpawner = {}
+
+function ASS_ObjectSpawner:Create()
+
+    self.spawnPoint = Property.Create(DatumType.Node, nil)
+
+end
+
+function ASS_ObjectSpawner:Interact()
+
+    -- Create new mesh node and give it physics
+    local newMeshNode = Node.Construct("StaticMesh3D")
+    newMeshNode:SetStaticMesh("SM_Cube")
+    newMeshNode:EnableCollision(true)
+    newMeshNode:EnablePhysics(true)
+    newMeshNode:SetScale(Vec(3, 3, 3))
+
+    -- Position it
+    local spawnPos = self.spawnPoint:GetWorldPosition()
+
+    newMeshNode:SetWorldPosition(spawnPos)
+    newMeshNode:SetRotation(Math.RandRangeVec(Vec(0, 0, 0), Vec(360, 360, 360)))
+
+    -- Add it to the world
+    self.world:GetRootNode():AddChild(newMeshNode)
+
+    -- Give it some random velocity
+    local velocity = Math.RandRangeVec(Vec(-5, -5, -5), Vec(5, 5, 5))
+    newMeshNode:SetLinearVelocity(velocity)
+
+
+end
+
+function ASS_ObjectSpawner:GetInteractionHint()
+    return "Spawn physics Object"
+end
