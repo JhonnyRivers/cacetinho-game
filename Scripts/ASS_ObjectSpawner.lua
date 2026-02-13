8260d37b9fc484ef686ec0b1a178e3907cffbf9b
JhonnyRivers <68939436+JhonnyRivers@users.noreply.github.com>
2026-02-13 14:04:50 -0300

    meteriais renomeados
    os materiais foram renomeados e sistema de spawn maximo adicionado, não funcionando completamente ainda, reseta o numero especificado no editor

--- Scripts/ASS_ObjectSpawner.lua
+++ Scripts/ASS_ObjectSpawner.lua
@@ -2,11 +2,27 @@ ASS_ObjectSpawner = {}
 
 function ASS_ObjectSpawner:Create()
 
-    self.spawnPoint = Property.Create(DatumType.Node, nil)
+
+    self.objetosAtivos = 0
+
+end
+
+function ASS_ObjectSpawner:GatherProperties()
+
+    return
+    {
+        { name = "spawnPoint", type = DatumType.Node },
+        { name = "quantidadePermitida", type = DatumType.Integer, },
+        
+    }
 
 end
 
 function ASS_ObjectSpawner:Interact()
+
+    if self.objetosAtivos >= self.quantidadePermitida then
+        return
+    end
 
     -- Create new mesh node and give it physics
     local newMeshNode = Node.Construct("StaticMesh3D")
@@ -18,16 +34,17 @@ function ASS_ObjectSpawner:Interact()
     -- Position it
     local spawnPos = self.spawnPoint:GetWorldPosition()
 
+
     newMeshNode:SetWorldPosition(spawnPos)
     newMeshNode:SetRotation(Math.RandRangeVec(Vec(0, 0, 0), Vec(360, 360, 360)))
 
     -- Add it to the world
     self.world:GetRootNode():AddChild(newMeshNode)
+    self.objetosAtivos = self.objetosAtivos + 1
 
     -- Give it some random velocity
     local velocity = Math.RandRangeVec(Vec(-5, -5, -5), Vec(5, 5, 5))
     newMeshNode:SetLinearVelocity(velocity)
-
 
 end
 
