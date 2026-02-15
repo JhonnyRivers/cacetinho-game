5acce99c21c09cc2a5b1d71bb76e6cb84c1755ca
Antonio Carlos Fernandes Gomes Filho <antoniocarlosfilho2003@gmail.com>
2026-02-14 21:40:05 -0300

    Commit: codigo plataforma (requer teste)

--- Scripts/trigger_Plataforma.lua
+++ Scripts/trigger_Plataforma.lua
@@ -27,9 +27,44 @@ function trigger_Plataforma:GatherPropeties()
 end
 
 function trigger_Plataforma:Start()
+    self.origin = self.plataforma:GetWorldPosition()
+
+    local direcao = self.direcaoXZ
+    direcao.y = 0
+    local comprimento = math.sqrt((direcao.x * direcao.x) + (direcao.z * direcao.z))
+    if comprimento == 0 then
+        self.direcaoXZ = Vec(1,0,0)
+    else
+        self.direcaoXZ = Vec((direcao.x / comprimento), 0, (direcao.z / comprimento)) 
+    end
 
 end
 
 function trigger_Plataforma:Tick(deltatime)
+
+    if not self.moving then
+        return
+    end
+
+    -- advance offset
+    self.offset = self.offset + (self.sign * self.velocidade * dt)
+
+    -- clamp & flip direction at ends
+    if self.offset > self.halfDistance then
+        self.offset = self.halfDistance
+        self.sign = -1
+    elseif self.offset < -self.halfDistance then
+        self.offset = -self.halfDistance
+        self.sign = 1
+    end
+
+    -- compute new world position (keep original Y)
+    local newPos = Vec(
+        self.origin.x + (self.dir.x * self.offset),
+        self.origin.y,
+        self.origin.z + (self.dir.z * self.offset)
+    )
+
+    self.node:SetWorldPosition(newPos)
 
 end
