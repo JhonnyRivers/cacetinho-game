5dc9d3e0a4bac3352d9d854e39809cc2fcb10e12
JhonnyRivers <68939436+JhonnyRivers@users.noreply.github.com>
2026-02-14 21:44:12 -0300

    sistema de moeda melhorado

--- Scripts/Moeda.lua
+++ Scripts/Moeda.lua
@@ -2,25 +2,21 @@ Moeda = {}
 
 function Moeda:Create()
 
-    self.coin = Property.Create(DatumType.Node, nil)
+    self.coinMesh = Property.Create(DatumType.Node, nil)
 
 end
 
-function Moeda:Interact()
+function Moeda:BeginOverlap(thisNode, otherNode)
 
-    if (self.coin) then
+    if (otherNode:HasTag("Player")) then
 
-        --local C_CON = self:GetParent()
-        --KCT_ThirdPersonController:SetScore(KCT_ThirdPersonController:GetScore() + 1)
+        local C_CON = self:GetParent()
+        KCT_ThirdPersonController:SetScore(KCT_ThirdPersonController:GetScore() + 1)
 
-        self.coin:SetVisible(not self.coin:IsVisible())
+        self.coinMesh:SetVisible(not self.coin:IsVisible())
     else
         Log.Warning("No coin assigned to switch")
     end
 
 
-end
-
-function Moeda:GetInteractionHint()
-    return "area de colisao modeda"
 end
